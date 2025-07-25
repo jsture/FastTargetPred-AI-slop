782419feb97de8cdef5fd0965786a9fd37b43eb4
jsture <jakobsturemadsen@gmail.com>
2025-07-25 14:56:09 +0200

    Update core Python files to v2.0
    - Update FastTargetPred.py with modern Python 3.8+ syntax
    - Update setup.py for ARM64 compatibility and modern packaging

--- FastTargetPred.py
+++ FastTargetPred.py
@@ -1,13 +1,14 @@
-__version__ = "1.2"
+__version__ = "2.0.0"
 
-# Python 3.7 Built-in packages
-import time
+# Python 3.8+ Built-in packages
 import sys
+import time
 
-try:
-    assert float(sys.version_info[0].__str__() + "." + sys.version_info[1].__str__()) >= 3.7
-except AssertionError:
-    print("This program only works with Python 3.7+ version. Please install a newer python version.")
+# Check Python version using modern syntax
+if sys.version_info < (3, 8):
+    print(
+        "This program requires Python 3.8 or higher. Please upgrade your Python version."
+    )
     sys.exit(1)
 
 # Local packages
@@ -15,24 +16,22 @@ from src import texts
 from src.misc import system_verification, clean_up
 from src.workflow import start
 
-def main():
+
+def main() -> None:
+    """Main entry point for FastTargetPred."""
     print(texts.app_header.format(__version__))
-    print(texts.check_system, end='')
+    print(texts.check_system, end="")
     system_ok, messages = system_verification()
     if system_ok:
         print(texts.checked)
         start()
     else:
-        print(texts.system_not_ok + '\n\t'.join(messages))
+        print(texts.system_not_ok + "\n\t".join(messages))
     clean_up()
-
 
 
 if __name__ == "__main__":
     begin_time = time.time()
     main()
     end_time = time.time()
-    print("Elapsed time for the all script : {}.".format(end_time - begin_time))
-
-
-
+    print(f"Elapsed time for the entire script: {end_time - begin_time:.2f} seconds.")
--- setup.py
+++ setup.py
@@ -1,52 +1,58 @@
-# Python 3.7 Built-in packages
-from distutils.core import setup, Extension
-from shutil import copy, rmtree
+# Python 3.8+ Built-in packages
+from setuptools import setup, Extension
+from shutil import rmtree
 from pathlib import Path
 import sys
 import argparse
+from typing import Any
 from src.misc import is_mac, is_linux, is_windows
 
 
-def compile(args):
+def compile_extension(args: Any) -> None:
+    """Compile the C extension for fingerprint processing."""
     include_dir = Path(args.python_h).as_posix()
     c_source = args.s
-    print(f"Directory chosen for Python.h : {include_dir}")
+    print(f"Directory chosen for Python.h: {include_dir}")
+
     # Name the package accordingly to the OS
-    file_name = 'tanimoto_processing'
+    file_name = "tanimoto_processing"
     if is_windows():
         file_name = "clib_w64." + file_name
     elif is_linux():
         file_name = "clib_linux64." + file_name
     elif is_mac():
         file_name = "clib_mac64." + file_name
+
     module1 = Extension(
-        'src.'+file_name
-        , sources=[c_source]
-        , include_dirs=[include_dir]
+        "src." + file_name, sources=[c_source], include_dirs=[include_dir]
     )
 
     setup(
-        name='tanimoto_processing'
-        , version='1.2'
-        , description='Fingerprint processing package.'
-        , ext_modules=[module1]
-        , author="Valentin Guillaume, Ludovic Chaput"
-        , packages=["src"]
+        name="tanimoto_processing",
+        version="2.0.0",
+        description="Fingerprint processing package.",
+        ext_modules=[module1],
+        author="Valentin Guillaume, Ludovic Chaput",
+        packages=["src"],
     )
+
     build_dir = Path(Path(sys.argv[0]).parent, "build")
     if build_dir.is_dir():
         rmtree(build_dir)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Script used to compile the C library into a callable python module.")
+    parser = argparse.ArgumentParser(
+        description="Script used to compile the C library into a callable python module."
+    )
     parser.add_argument("python_h", help="Python.h header file location")
-    parser.add_argument("-s", help="Source file location", default="src/tanimoto_processing.c")
-    # parser.add_argument("--build-lib", help="Do not ", default=".", dest="d")
+    parser.add_argument(
+        "-s", help="Source file location", default="src/tanimoto_processing.c"
+    )
 
     args, unknown = parser.parse_known_args()
 
-    # Changing the sysargv for distutil.core.setup
-    sys.argv = [*sys.argv[:1], "build", "--build-lib", '.']
+    # Changing the sys.argv for distutil.core.setup
+    sys.argv = [*sys.argv[:1], "build", "--build-lib", "."]
 
-    compile(args)
+    compile_extension(args)
